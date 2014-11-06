8c415ab653edf5af75e58e088ca34e6168998bdd
thatknitter <kblewis.flt@gmail.com>
2014-11-06 15:35:16 -0600

    bower, mocha and chai files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
-$(function(){
-	var $name = $('name');
+document.addEventListener("DOMContentLoaded", function(){
+  var $form = document.querySelector('form');
+  var $name = $form.querySelector("input[type='text']"); 
+  $form.addEventListener("submit", function(event){
+    event.preventDefault();
+    
+  });
+    
 });
+
+function nameArray(name){
+	//array=[0]
+	return name.split('');
+}
+
+function capitalName(splitName){
+	return _.map(splitName, function(item, index){
+		return item.toUpperCase();
+	});
+}
+
+function cheer(list, items){
+	var grammarA = ["B", "C", "D", "G", "J", "K", "P", "Q", "T", "U", "V", "W", "Y", "Z"];
+	var grammarAn = ["A", "E", "F", "H", "I", "L", "M", "N", "O", "R", "S", "X"];
+	var gimme = "Give me";
+	for(var i = capitalName().length-1; i--;){
+	if (_.intersection(capitalName[0], grammarA){
+		return gimme + " a " + _.first.capitalName;
+		capitalName.splice(i, 1);
+	} else {
+		return gimme + " an " + _.first.capitalName;
+		capitalName.splice(i, 1);
+	}
+	}
+}
--- js/tests.js
+++ js/tests.js
@@ -1,3 +1,23 @@
 var q = QUnit;
 
+q.test("split function", function(assert){
+	var name = "Kimberly";
+	var splitName = ["K","i","m","b","e","r","l","y"];
+	
+	assert.deepEqual(nameArray(name), splitName);
+});
 
+q.test("make the letter caps", function(assert){
+	var name = "Kimberly";
+	var splitName = ["K","i","m","b","e","r","l","y"];
+	var capName = ["K", "I", "M", "B", "E", "R", "L", "Y"];
+	
+	assert.deepEqual(capitalName(splitName), capName);
+});
+
+q.test("run cheer", function(assert){
+	var capitalName = ["B", "O", "B"];
+	var cheerName = ["Give me a B", "Give me an o", "Give me a b"];
+	
+	assert.deepEqual(cheer(capitalName), cheerName);
+});
